181b186405e9b221a06716733c7647be41916c9b
LZHTK <gusdnjsrl2@gmail.com>
2025-09-15 09:37:14 +0900

    [SCRUM-55] feat: ChangePasswordRequest Dto 작성

---
+++ src/main/java/com/sprint/otboo/user/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,14 @@
+package com.sprint.otboo.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangePasswordRequest(
+    @NotBlank(message = "현재 비밀번호는 필수입니다")
+    String currentPassword,
+    @NotBlank(message = "새 비밀번호는 필수입니다")
+    @Size(min = 8, max = 255, message = "새 비밀번호는 8자 이상 255자 이하여야 합니다")
+    String password
+) {
+
+}
