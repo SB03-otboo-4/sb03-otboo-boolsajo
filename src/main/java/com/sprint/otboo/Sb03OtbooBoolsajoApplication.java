6475ea63362265289df5dcd49c3ff27349700b99
bs8841 <whqortjsgkgk@naver.com>
2025-09-17 10:51:50 +0900

    [SCRUM-59] chore: [Green] configuration-processor 추가

--- src/main/java/com/sprint/otboo/Sb03OtbooBoolsajoApplication.java
+++ src/main/java/com/sprint/otboo/Sb03OtbooBoolsajoApplication.java
@@ -2,8 +2,10 @@ package com.sprint.otboo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Sb03OtbooBoolsajoApplication {
 
 	public static void main(String[] args) {
