f84a9a60b8ef31e9bfbbef81504895cb1a2267b8
bs8841 <whqortjsgkgk@naver.com>
2025-09-17 10:53:25 +0900

    [SCRUM-59] feat: [Green] Kakao WebClient 빈 구성

---
+++ src/main/java/com/sprint/otboo/common/config/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.sprint.otboo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean(name = "kakaoWebClient")
+    public WebClient kakaoWebClient(KakaoApiProperties props) {
+        return WebClient.builder()
+            .baseUrl(props.baseUrl())
+            .defaultHeader(HttpHeaders.AUTHORIZATION, "KakaoAK " + props.restApiKey())
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+}
