cd47e4bd95b0e1ccca96388e3e8a4448a1f9bca1
LZHTK <gusdnjsrl2@gmail.com>
2025-09-17 13:31:45 +0900

    [SCRUM-53] fix: UserProfileRepository interface 수정

--- src/main/java/com/sprint/otboo/user/repository/UserProfileRepository.java
+++ src/main/java/com/sprint/otboo/user/repository/UserProfileRepository.java
@@ -3,8 +3,9 @@ package com.sprint.otboo.user.repository;
 import com.sprint.otboo.user.entity.UserProfile;
 import java.util.UUID;
 import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProfileRepository extends JpaAttributeConverter<UserProfile, UUID> {
+public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
 }
