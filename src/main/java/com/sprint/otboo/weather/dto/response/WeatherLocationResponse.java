c48dcd568544a75dc6b8669c7b127bdf0a19ade1
bs8841 <whqortjsgkgk@naver.com>
2025-09-19 17:56:01 +0900

    [SCRUM-59] docs: 날씨 위치 정보 조회 Swagger 문서 추가

--- src/main/java/com/sprint/otboo/weather/dto/response/WeatherLocationResponse.java
+++ src/main/java/com/sprint/otboo/weather/dto/response/WeatherLocationResponse.java
@@ -1,12 +1,22 @@
 package com.sprint.otboo.weather.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record WeatherLocationResponse(
+    @Schema(description = "위도", example = "37.5665")
     double latitude,
+
+    @Schema(description = "경도", example = "126.9780")
     double longitude,
+
+    @Schema(description = "x", example = "60")
     int x,
+
+    @Schema(description = "y", example = "127")
     int y,
+
+    @Schema(description = "지명", example = "[\"서울특별시\",\"중구\",\"태평로1가\"]")
     List<String> locationNames
 ) {
     public WeatherLocationResponse {
