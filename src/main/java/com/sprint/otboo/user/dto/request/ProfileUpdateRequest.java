a3c4468a8e283c77ea6a4caa4ab756ad5596dc44
LZHTK <gusdnjsrl2@gmail.com>
2025-09-22 14:14:24 +0900

    [SCRUM-54] feat: GREEN단계 - DTO 정리

--- src/main/java/com/sprint/otboo/user/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/sprint/otboo/user/dto/request/ProfileUpdateRequest.java
@@ -9,19 +9,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record ProfileUpdateRequest(
-    @NotBlank(message = "이름은 필수값입니다.")
-    String name,
-
-    @Size(max = 10, message = "성별은 10자 이하로 입력하세요.")
-    String gender,
-
+    @NotBlank(message = "이름은 필수값입니다.") String name,
+    @Size(max = 10, message = "성별은 10자 이하로 입력하세요.") String gender,
     LocalDate birthDate,
     BigDecimal latitude,
     BigDecimal longitude,
     Integer x,
     Integer y,
     List<String> locationNames,
-
     @Min(value = 0, message = "온도 민감도는 0 이상이어야 합니다.")
     @Max(value = 5, message = "온도 민감도는 5 이하여야 합니다.")
     Integer temperatureSensitivity
