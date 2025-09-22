8e2b49c6d7ee3bd7658d8b5c29135bae123350b0
Chaewon3Lee <coduszxcv@gmail.com>
2025-09-22 14:10:01 +0900

    [SCRUM-25] feat: 피드 수정 Controller Green 기능 구현

--- src/main/java/com/sprint/otboo/feed/controller/FeedController.java
+++ src/main/java/com/sprint/otboo/feed/controller/FeedController.java
@@ -1,13 +1,19 @@
 package com.sprint.otboo.feed.controller;
 
+import com.sprint.otboo.auth.jwt.CustomUserDetails;
 import com.sprint.otboo.feed.dto.data.FeedDto;
 import com.sprint.otboo.feed.dto.request.FeedCreateRequest;
+import com.sprint.otboo.feed.dto.request.FeedUpdateRequest;
 import com.sprint.otboo.feed.service.FeedService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +37,16 @@ public class FeedController implements FeedApi {
         log.info("[FeedController] 피드 생성 완료: feedId={}", dto.id());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
+
+    @PatchMapping("/{feedId}")
+    public ResponseEntity<FeedDto> update(
+        @PathVariable UUID feedId,
+        @AuthenticationPrincipal CustomUserDetails principal,
+        @Valid @RequestBody FeedUpdateRequest request
+    ) {
+        UUID authorId = principal.getUserId();
+        FeedDto dto = feedService.update(authorId, feedId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
 }
