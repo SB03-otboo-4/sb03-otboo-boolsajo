eaa3a4f9c50815414323b357689eefb116cdf48d
LZHTK <gusdnjsrl2@gmail.com>
2025-09-24 13:38:16 +0900

    [SCRUM-48] feat: Notification Entity 추가

--- src/main/java/com/sprint/otboo/notification/entity/Notification.java
+++ src/main/java/com/sprint/otboo/notification/entity/Notification.java
@@ -1,5 +1,43 @@
 package com.sprint.otboo.notification.entity;
 
-public class Notification {
+import com.sprint.otboo.common.base.BaseEntity;
+import com.sprint.otboo.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@Entity
+@Table(name = "notifications")
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+        name = "receiver_id",
+        nullable = false,
+        foreignKey = @ForeignKey(name = "fk_notifications_receiver")
+    )
+    private User user;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content", nullable = false, length = 100)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "level", nullable = false, length = 10)
+    private NotificationLevel level;
 }
--- src/main/java/com/sprint/otboo/user/entity/User.java
+++ src/main/java/com/sprint/otboo/user/entity/User.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
-import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
