5e67ab7d9b357503c16ec2a4ce85383dbd5ede92
LZHTK <gusdnjsrl2@gmail.com>
2025-09-29 15:14:16 +0900

    [SCRUM-74] feat: default admin 계정 생성 로직 추가

---
+++ src/main/java/com/sprint/otboo/user/config/DefaultAdminInitializer.java
@@ -0,0 +1,40 @@
+package com.sprint.otboo.user.config;
+
+import com.sprint.otboo.user.entity.LoginType;
+import com.sprint.otboo.user.entity.Role;
+import com.sprint.otboo.user.entity.User;
+import com.sprint.otboo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DefaultAdminInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner adminSeeder() {
+        return args -> {
+            String email = "boolsajo@otboo.io";
+            if (userRepository.existsByEmail(email)) {
+                return; // 이미 존재시 아무 것도 안함.
+            }
+
+            User user = User.builder()
+                .username("boolsajo")
+                .email(email)
+                .password(passwordEncoder.encode("boolsajo1!"))
+                .role(Role.ADMIN)
+                .locked(false)
+                .provider(LoginType.GENERAL)
+                .build();
+
+            userRepository.save(user);
+        };
+    }
+}
