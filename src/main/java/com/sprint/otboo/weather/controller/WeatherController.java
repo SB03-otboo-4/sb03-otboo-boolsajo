ccf184c4793e18520ea2fb6527bf5790d2a523a2
bs8841 <whqortjsgkgk@naver.com>
2025-09-29 10:07:40 +0900

    [SCRUM-58] feat: [Green] WeatherController 구현

--- src/main/java/com/sprint/otboo/weather/controller/WeatherController.java
+++ src/main/java/com/sprint/otboo/weather/controller/WeatherController.java
@@ -1,5 +1,26 @@
 package com.sprint.otboo.weather.controller;
 
+import com.sprint.otboo.weather.dto.data.WeatherSummaryDto;
+import com.sprint.otboo.weather.service.WeatherService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
 public class WeatherController {
 
+    private final WeatherService weatherService;
+
+    @GetMapping("/api/weathers")
+    public List<WeatherSummaryDto> getWeathers(
+        @RequestParam double latitude,
+        @RequestParam double longitude
+    ) {
+        return weatherService.getWeather(latitude, longitude);
+    }
 }
