0d9973840bb5eb014be6eafc638372de1389649c
LZHTK <gusdnjsrl2@gmail.com>
2025-09-29 16:58:01 +0900

    [SCRUM-74] fix: Default Admin 비밀번호 하드 코딩된 부분 수정

--- src/main/java/com/sprint/otboo/user/config/DefaultAdminInitializer.java
+++ src/main/java/com/sprint/otboo/user/config/DefaultAdminInitializer.java
@@ -5,6 +5,7 @@ import com.sprint.otboo.user.entity.Role;
 import com.sprint.otboo.user.entity.User;
 import com.sprint.otboo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,8 @@ public class DefaultAdminInitializer {
     private final PasswordEncoder passwordEncoder;
 
     @Bean
-    public CommandLineRunner adminSeeder() {
+    public CommandLineRunner adminSeeder(
+        @Value("${admin.default.password}") String defaultPassword) {
         return args -> {
             String email = "boolsajo@otboo.io";
             if (userRepository.existsByEmail(email)) {
@@ -28,7 +30,7 @@ public class DefaultAdminInitializer {
             User user = User.builder()
                 .username("boolsajo")
                 .email(email)
-                .password(passwordEncoder.encode("boolsajo1!"))
+                .password(passwordEncoder.encode(defaultPassword))
                 .role(Role.ADMIN)
                 .locked(false)
                 .provider(LoginType.GENERAL)
