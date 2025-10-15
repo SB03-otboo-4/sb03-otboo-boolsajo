125ade81a796610ef36b9df1d8442fab20b06ebb
GeonMin02 <geonmin10@gmail.com>
2025-10-15 10:01:02 +0900

    [SCRUM-69] test: NotificationRepositoryTest 시나리오 추가 작성

--- src/main/java/com/sprint/otboo/notification/repository/NotificationRepository.java
+++ src/main/java/com/sprint/otboo/notification/repository/NotificationRepository.java
@@ -4,8 +4,15 @@ import com.sprint.otboo.notification.entity.Notification;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID>, NotificationRepositoryCustom {
     long countByReceiverId(UUID receiverId);
-    List<Notification> findByReceiverIdAndIdAfter(UUID receiverId, UUID id);
+
+    @Query("select n from Notification n where n.receiver.id = :receiverId "
+        + "and (:lastId is null or n.id > :lastId)")
+    List<Notification> findByReceiverIdAndIdAfter(
+        @Param("receiverId") UUID receiverId,
+        @Param("lastId") UUID lastId);
 }
