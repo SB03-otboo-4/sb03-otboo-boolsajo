b64d7999759208499adc5fad9bbdbd0e4bcfbd82
bs8841 <whqortjsgkgk@naver.com>
2025-10-22 15:38:59 +0900

    [SCRUM-61] fix: 팔로우 생성 문제 수정

--- src/main/java/com/sprint/otboo/follow/dto/request/FollowCreateRequest.java
+++ src/main/java/com/sprint/otboo/follow/dto/request/FollowCreateRequest.java
@@ -1,8 +1,10 @@
 package com.sprint.otboo.follow.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FollowCreateRequest(
     @NotNull(message = "followeeId는 필수입니다.") UUID followeeId
 ) {}
